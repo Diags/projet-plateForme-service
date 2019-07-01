e010ce893fb7c3f3a383b9fb0dd7454b0075f1be
dsylla <diaguily.sylla@agixis.com>
2019-07-01 17:35:47 +0200

    fifteen  commits

--- frontEnd/front/src/app/acceuil/acceuil.component.ts
+++ frontEnd/front/src/app/acceuil/acceuil.component.ts
@@ -8,15 +8,13 @@ import {el} from "@angular/platform-browser/testing/src/browser_util";
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
-export class AcceuilComponent implements OnInit, AfterViewInit {
+export class AcceuilComponent implements OnInit {
   users;
   categories;
   currenteCategorie;
   curenteSearchUsers;
   iscontactChecked = false;
   currentUser;
-  svgTagId;
-  @ViewChild('mapSen') mapSen: ElementRef;
 
   constructor(private catelogService: CatalogueService, private  routeActive: ActivatedRoute, private route: Router) {
   }
@@ -61,60 +59,5 @@ export class AcceuilComponent implements OnInit, AfterViewInit {
   getUsersBycategories(c) {
     this.currenteCategorie = c;
     this.route.navigateByUrl('/catalogue/2/' + c.id);
-  }
-
-  ngAfterViewInit(): void {
-    let map = this.mapSen.nativeElement.querySelector("div");
-    console.log("mapppp", map);
-    let mapb = this.mapSen.nativeElement.querySelector("#map_s");
-    let paths = map.querySelectorAll('svg a');
-    console.log("this is opaths values ", paths);
-    console.log("list right", map.querySelector('#map_list').querySelectorAll('a'));
-    let links = map.querySelector('#map_list').querySelectorAll('a');
-
-    console.log("map_list*****", links);
-    console.log("mapSen////////", paths);
-    console.log("map##", map);
-
-    //polyfill du foreach
-    if (NodeList.prototype.forEach === undefined) {
-      NodeList.prototype.forEach = function (callback) {
-        [].forEach.call(this.callback)
-      }
-    }
-
-    paths.forEach(path => {
-      path.addEventListener("mouseenter", function (e) {
-        let id = this.id;
-        extracted(id);
-      })
-    });
-    links.forEach(link => {
-      link.addEventListener("mouseenter", function () {
-        let id = this.id.replace("list-", "");
-        extracted(id);
-
-      })
-    });
-    map.addEventListener("mouseover", function (e) {
-      let id;
-      extracted(id);
-
-    });
-
-    function extracted(id) {
-      map.querySelectorAll('.is-active').forEach(e => {
-        e.classList.remove('is-active')
-      });
-      if (id != undefined) {
-        map.querySelector('#map_list ' + '#list-' + id).classList.add('is-active');
-        map.querySelector('svg #' + id).classList.add('is-active');
-      }
-    }
-
-  }
-
-  clickTo(da){
-    console.log("ddddkffopd",da);
   }
 }
